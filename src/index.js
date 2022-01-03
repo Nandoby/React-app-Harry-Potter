d38a116db8d61f13282f01f3e1f42e73098574ae
nuageblanc7000 <wetterene.remy@gmail.com>
2022-01-03 19:04:31 +0100

    ajout pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Rooter, Route, Router} from 'react-router-dom';
+import {Serpentard as Serp , Gryffondor as Grif,Poufsouffle as Pouf, Serdaigle as Serd} from "./pages"
+function App(){
+  <Router>
+    <Routes>
+      <Route path={"/"} component={App} />
+      <Route path={"/"} component={App} />
+    </Routes>
+  </Router>
+}
 
 ReactDOM.render(
   <React.StrictMode>
