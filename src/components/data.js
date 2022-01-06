71d903e74d4522c441ddbc86380a78e33ceaf85c
Nandoby <nandobiaccalli@gmail.com>
2022-01-06 23:03:48 +0100

    state externe

---
+++ src/components/data.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    school: 'Gryffondor',
+    coverImg: 'https://www.piwwie.com/wp-content/uploads/2020/04/gryff.jpg',
+  },
+  {
+    school: 'Serpentard',
+    coverImg: 'https://www.piwwie.com/wp-content/uploads/2020/04/serpentard.jpg'
+  },
+  {
+    school: 'Poufsouffle',
+    coverImg: 'https://www.piwwie.com/wp-content/uploads/2020/04/poufsouffle.jpg',
+    presentation: 'La création de cette maison revient à <b>Helga Poufsouffle</b>, lors de la fondation de Poudlard. Cette dernière fait figure de modèle maternel encore aujourd’hui. À l’époque, elle rassembla des gens aux modes de vie différents pour aider à construire Poudlard et était aimée pour ses manières charmantes. En outre, Helga Poufsouffle était particulièrement connue pour ses sortilèges appliqués à la nourriture. D’ailleurs, un bon nombre de recettes encore utilisées traditionnellement pour les fêtes de Poudlard proviennent d’elle. Elle a valorisé le sens du partage et la bienveillance et a donc souhaité que tous les élèves de sa Maison partage ces qualités. Par ailleurs, il s’agissait d’une grande amie de Rowena Serdaigle. Elle était originaire des vallée galloises et vécu, comme les trois autres fondateurs, au 10ème siècle.'
+  },
+  {
+    school: 'Serdaigle',
+    coverImg: 'https://www.piwwie.com/wp-content/uploads/2020/04/serdaigle.jpg'
+  }
+]
